5012c601865d67395c59e6526343c2ac9a1e6140
EC2 Default User <ec2-user@ip-172-31-20-122.us-east-2.compute.internal>
2018-03-22 10:32:28 +0000

    test

--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -2,10 +2,12 @@
     @if (Auth::user()->is_following($user->id))
         {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfollow', ['class' => "btn btn-danger btn-block"]) !!}
+            {{ csrf_field() }}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
             {!! Form::submit('Follow', ['class' => "btn btn-primary btn-block"]) !!}
+            {{ csrf_field() }}
         {!! Form::close() !!}
     @endif
 @endif
